903178e68d1b986ff062312f690cd17e8bfcdd4b
Anik-the-dev <niksctg00@gmail.com>
2023-07-18 08:48:48 +0600

    token generation done

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,6 +1,39 @@
+const People = require("../models/People");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 // Login Page
 function getLogin(req, res, next) {
   res.render("index");
+}
+
+//  do login
+// validate user( by user input) if pass: generate token, save to cookie, send the cookie, inbox render
+async function login(req, res, next) {
+  const user = await People.findOne({
+    $or: [{ email: res.body.username }, { mobile: res.body.username }],
+  });
+  if (user && user._id) {
+    const validatedPassword = await bcrypt.compare(
+      res.body.password,
+      user.password
+    );
+    if (validatedPassword) {
+      // userdata
+      const payload = {
+        name: user.name,
+        email: user.email,
+        mobile: user.mobile,
+        role: "user",
+      };
+      // generate token
+      const token = jwt.sign(payload, process.env.TOKEN_KEY_SECRET, {
+        expiresIn: "24hr",
+      });
+
+      // set cookie and send cookie
+    }
+  }
 }
 module.exports = {
   getLogin,
--- test_envoz.js
+++ test_envoz.js
@@ -1,1 +1,18 @@
-console.log("test_envoz");
+// console.log("test_envoz");
+// const bcrypt = require("bcrypt");
+// let hp = {};
+// const hashedPassword = bcrypt
+//   .hash("Anik1995$", 3)
+//   .then((pass) => ((hp.pass = pass), console.log(hp)));
+
+// setTimeout(() => {
+//   const validatedPassword = bcrypt.compare("Anik1995$", hp.pass);
+//   validatedPassword.then((res) => console.log(res));
+// }, 1000);
+
+// jwt
+const jwt = require("jsonwebtoken");
+let a = jwt.sign({ name: "ANIK" }, "a4534534534sdsda3", {
+  expiresIn: "24hr",
+});
+console.log(a);
